4f22354368c10b674ab3f0b75c31e23cb3feaa9e
Evangel Daping <evangeldaping@gmail.com>
2025-01-25 20:36:52 +0000

    Rollup failed to resolve import react-router-dom
    Install react router dom
    Add rollupOptions for vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        // Node.js built-ins (if your app uses server-side code)
+        'fs', 'path', 'os',
+
+        // External libraries you want to exclude from the bundle
+        'axios', 'lodash',
+
+        // Dependencies to leave out of the bundle
+        'react', 'react-dom', // For example, if you're using React and including it via CDN
+      ],
+    },
+  },
   plugins: [react()],
 })
