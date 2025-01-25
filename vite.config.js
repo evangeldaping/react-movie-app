79570e9772a6978e5dea35a4484b8f337f2ea172
Evangel Daping <evangeldaping@gmail.com>
2025-01-25 21:06:14 +0000

    Update vite
    Remove unused vite config

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        // Node.js built-ins (if your app uses server-side code)
-        'fs', 'path', 'os',
-
-        // External libraries you want to exclude from the bundle
-        'axios', 'lodash',
-
         // Dependencies to leave out of the bundle
         'react', 'react-dom', // For example, if you're using React and including it via CDN
       ],
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      // If you're using SCSS/SASS or other preprocessors, ensure they are configured
     },
   },
   plugins: [react()],
